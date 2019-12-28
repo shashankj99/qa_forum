c2163857a748a52fcc4b1176a9ec63702de0b3e4
Shashank Jha <shashankj677@gmail.com>
2019-12-28 09:30:56 +0545

    generated fake answers

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('questions.index');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function () {
 
-// Question Controller
-Route::resource('questions', 'QuestionsController')->except('show');
-Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    // Question Controller
+    Route::resource('questions', 'QuestionsController')->except('show');
+    Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
+
+});
+
